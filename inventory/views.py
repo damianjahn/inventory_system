6eb441cb0c53c69835dd130af4cd2dd326a8db49
Damian <damian.jahn@icloud.com>
2020-07-29 13:24:10 +0200

    Adding the edit button

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
 
@@ -58,3 +58,68 @@ def add_desktop(request):
 
 def add_mobile(request):
     return add_item(request, MobileForm)
+
+
+def edit_item(request, pk, model, cls):
+    item = get_object_or_404(model, pk=pk)
+
+    if request.method == "POST":
+        form = cls(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = cls(instance=item)
+
+        return render(request, 'edit_item.html', {'form': form})
+
+
+def edit_laptop(request, pk):
+    return edit_item(request, pk, Laptop, LaptopForm)
+
+
+def edit_desktop(request, pk):
+    return edit_item(request, pk, Desktop, DesktopForm)
+
+
+def edit_mobile(request, pk):
+    return edit_item(request, pk, Mobile, MobileForm)
+
+
+def delete_laptop(request, pk):
+    template = 'inv/index.html'
+    Laptop.objects.filter(id=pk).delete()
+
+    items = Laptop.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, template, context)
+
+
+def delete_desktop(request, pk):
+    template = 'inv/index.html'
+    Desktop.objects.filter(id=pk).delete()
+
+    items = Desktop.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, template, context)
+
+
+def delete_mobile(request, pk):
+    template = 'inv/index.html'
+    Mobile.objects.filter(id=pk).delete()
+
+    items = Mobile.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, template, context)
