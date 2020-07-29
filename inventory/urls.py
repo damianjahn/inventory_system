db79252cd85a3a28d2e802eecfcf7a5c80fbb5ef
Damian <damian.jahn@icloud.com>
2020-07-29 12:12:00 +0200

    bug fixes

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     url(r'^display_laptops$', display_laptops, name='display_laptops'),
     url(r'^display_desktops$', display_desktops, name='display_desktops'),
     url(r'^display_mobiles$', display_mobiles, name='display_mobiles'),
-    url(r'^add_laptop$', add_laptop(), name='add_laptop'),
-    url(r'^desktop$', add_desktop(), name='add_desktop'),
-    url(r'^add_mobile$', add_mobile(), name='add_mobile')
+
+    url(r'^add_laptop$', add_laptop, name='add_laptop'),
+    url(r'^add_desktop$', add_desktop, name='add_desktop'),
+    url(r'^add_mobile$', add_mobile, name='add_mobile')
 ]
