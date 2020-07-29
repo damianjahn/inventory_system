71f8926b8b12c6fb506357c60bed27180ecfa662
Damian <damian.jahn@icloud.com>
2020-07-29 11:57:44 +0200

    Defining forms and views, bug fixes

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^display_laptops$', display_laptops, name='display_laptops'),
     url(r'^display_desktops$', display_desktops, name='display_desktops'),
-    url(r'^display_mobiles$', display_mobiles, name='display_mobiles')
+    url(r'^display_mobiles$', display_mobiles, name='display_mobiles'),
+    url(r'^add_laptop$', add_laptop(), name='add_laptop'),
+    url(r'^desktop$', add_desktop(), name='add_desktop'),
+    url(r'^add_mobile$', add_mobile(), name='add_mobile')
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 
 # Create your views here.
@@ -11,6 +12,7 @@ def display_laptops(request):
     items = Laptop.objects.all()
     context = {
         'items': items,
+        'header': 'Laptops'
     }
     return render(request, 'index.html', context)
 
@@ -19,6 +21,7 @@ def display_desktops(request):
     items = Desktop.objects.all()
     context = {
         'items': items,
+        'header': 'Desktops'
     }
     return render(request, 'index.html', context)
 
@@ -27,5 +30,31 @@ def display_mobiles(request):
     items = Mobile.objects.all()
     context = {
         'items': items,
+        'header': 'Mobiles'
     }
     return render(request, 'index.html', context)
+
+
+def add_item(request, cls):
+    if request.method == "POST":
+        form = cls(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    else:
+        form = cls()
+        return render(request, 'add_new.html', {'form': form})
+
+
+def add_laptop(request):
+    return add_item(request, LaptopForm)
+
+
+def add_desktop(request):
+    return add_item(request, DesktopForm)
+
+
+def add_mobile(request):
+    return add_item(request, MobileForm)
